af6b2983f2f3bb265d62d8ad8540226d5420e3f6
yutasasao <ss.yuta@icloud.com>
2020-05-06 16:52:44 +0900

    third commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,15 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  username :ENV["BASIC_AUTH_USER"]
+  password :ENV["BASIC_AUTH_PASSWORD"]
+}
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,4 @@
 server '3.113.160.7', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
