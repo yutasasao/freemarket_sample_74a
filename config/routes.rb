1709160e31057eb5259e6a4cae4525b4649dc35a
kodai fukui <kodai1863@gmail.com>
2020-05-14 11:41:00 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tops#show'
+  root to: 'tops#index'
+  resources :sign_up, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
