b471579299382a0b6b97f90d403662273a1387e9
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-05-19 18:32:20 +0900

    user_controller_edit
    user_controllerのshowアクションの変数@usersを@userに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.where(user_id: current_user.id)
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
 end
