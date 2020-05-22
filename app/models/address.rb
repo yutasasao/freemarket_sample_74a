46e876f208262e8341b50a0c2d768ac2b5792134
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-05-22 15:21:17 +0900

    error_message_update
    エラーメッセージの表示及び
    パスワードバリデーションの文字数制限を修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,14 +7,14 @@ class Address < ApplicationRecord
   validates :postal_code, :prefecture_id, :city, :street, :first_name, :last_name, :first_name_kana, :last_name_kana, presence: true
   validates :first_name_kana, :last_name_kana, format: {
     with: /\A[\p{hiragana}　ー－&&[^ -~｡-ﾟ]]+\z/,
-    message: "全角ひらがなのみで入力して下さい"
   }
   validates :first_name, :last_name, format: {
     with: /[^ -~｡-ﾟ]+/,
-    message: "全角で入力して下さい"
+  }
+  validates :prefecture_id,format: {
+    with: /[1-47]+/,
   }
   validates :postal_code, format: {
     with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/,
-    message: "郵便番号を入力してください"
   }
 end
